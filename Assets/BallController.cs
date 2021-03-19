a1a90fc2eb30e6b065e32cec739613bec759d1f4
akiyoorange <79643871+akiyoorange@users.noreply.github.com>
2021-03-19 18:00:54 +0900

    Initial commit

---
+++ Assets/BallController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallController : MonoBehaviour
+{
+    //ボールが見える可能性のあるz軸の最大値
+    private float visiblePosZ = -6.5f;
+
+    //得点を表示するテキスト
+    private GameObject scoreText;
+
+    //各得点の初期化
+    int scoreA = 0;
+    int scoreB = 0;
+    int scoreC = 0;
+    int scoreD = 0;
+
+    void Start() 
+    {
+        //シーン中のGameOverTextオブジェクトを取得
+        this.scoreText = GameObject.Find("ScoreText");
+
+    }
+       
+　　//衝突時の判定
+　　void OnCollisionEnter(Collision collision) 
+    {
+　　　　 //衝突時に各Tagの得点を加算
+        if (collision.gameObject.tag == "SmallStarTag")
+        {
+            scoreA += 5;
+            Debug.Log("小さい星に当った");
+        }
+        else if (collision.gameObject.tag == "LargrStarTag")
+        {
+            scoreB += 15;
+            Debug.Log("大きい星に当った");
+        }
+        else if (collision.gameObject.tag == "SmallCloudTag")
+            {
+            scoreC += 10;
+            Debug.Log("小さい雲に当った");
+        }
+        else if (collision.gameObject.tag == "LargeCloudTag")
+        {
+                scoreD += 20;
+            Debug.Log("大きい雲に当った");
+        }       
+    }
+
+    void Update()
+    {
+        //ボールが画面外に出た場合の処理
+        if (this.transform.position.z < this.visiblePosZ)
+        {
+            //各得点の合計値を出す
+            int totalscore = scoreA + scoreB + scoreC + scoreD;
+
+            //GameoverTextに合計値を表示
+            this.scoreText.GetComponent<Text>().text = "Your Score is "+ totalscore;
+        }
+    }
+
+    //衝突時に呼び出される関数
+    void OnCollisionEnter() { }
+}
+
---
+++ Assets/FripperController.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FripperController : MonoBehaviour
+{
+
+    //HingeJointコンポーネントを入れる
+    private HingeJoint myHingeJoint;
+
+    //初期の傾き
+    private float defaultAngle = 20;
+    //弾いた時の傾き
+    private float flickAngle = -20;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //HingeJointコンポーネント取得
+        this.myHingeJoint = GetComponent<HingeJoint>();
+
+        //フリッパーの傾きを設定
+        SetAngle(this.defaultAngle);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //タッチされているかの判断
+        if (Input.touchCount > 0)
+        {
+            //スクリーンの右半分をタップした際の処理
+            if (Input.mousePosition.x >= Screen.width / 2)
+            {
+                //タップしたら右フリッパーを動かす
+                if (Input.touches[0].phase == TouchPhase.Began && tag == "RightFripperTag")
+                {
+                    SetAngle(this.flickAngle);
+                }
+                //指を離したら右フリッパーを元に戻す
+                if (Input.touches[0].phase == TouchPhase.Ended && tag == "RightFripperTag")
+                {
+                    SetAngle(this.defaultAngle);
+                }
+            }
+           
+            //右半分以外をタップした際の処理
+            else 
+            {
+                //タップしたら左フリッパーを動かす
+                if (Input.touches[0].phase == TouchPhase.Began && tag == "LeftFripperTag")
+                {
+                    SetAngle(this.flickAngle);
+                }
+                //指を離したら左フリッパーを元に戻す
+                if (Input.touches[0].phase == TouchPhase.Ended && tag == "LeftFripperTag")
+                {
+                    SetAngle(this.defaultAngle);
+                }
+            }
+        }
+        //タッチされているかの判断
+        if (Input.touchCount > 1)
+        {
+            if (Input.touches[1].phase == TouchPhase.Began && tag == "RightFripperTag")
+            {
+                SetAngle(this.flickAngle);
+            }
+            if (Input.touches[1].phase == TouchPhase.Ended && tag == "RightFripperTag")
+            {
+                SetAngle(this.defaultAngle);
+            }
+            if (Input.touches[1].phase == TouchPhase.Began && tag == "LeftFripperTag")
+            {
+                SetAngle(this.flickAngle);
+            }
+            //指を離したら左フリッパーを元に戻す
+            if (Input.touches[1].phase == TouchPhase.Ended && tag == "LeftFripperTag")
+            {
+                SetAngle(this.defaultAngle);
+            }
+        }
+    }
+
+    //フリッパーの傾きを設定
+    public void SetAngle(float angle)
+    {
+        JointSpring jointSpr = this.myHingeJoint.spring;
+        jointSpr.targetPosition = angle;
+        this.myHingeJoint.spring = jointSpr;
+    }
+}
